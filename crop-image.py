2a046d7849dbc2111df5ae733a422387024dd62c
XHXIAIEIN <45864744+XHXIAIEIN@users.noreply.github.com>
2021-08-11 13:50:25 +0800

    Update crop-image.py

--- crop-image.py
+++ crop-image.py
@@ -14,15 +14,20 @@ BOX_UP,BOX_LEFT,BOX_DOWN,BOX_RIGHT,= 470, 0, 828, 828
 if not os.path.exists(IMAGE_OUTPUT_PATH):
     os.makedirs(IMAGE_OUTPUT_PATH)
 
+# 开始遍历文件夹内的所有文件
 for each_image in os.listdir(IMAGE_INPUT_PATH):
+    
+    # 补全文件的完整路径
     image_input_fullname = IMAGE_INPUT_PATH + '/' + each_image
-
-    # 过滤其他类型的文件
+    
+    # 判断文件类型
     image_type = os.path.splitext(each_image)[-1].lower()
-
+    
+    # 过滤其他类型的文件
     if image_type not in ['.jpg', '.jpeg', '.png']: 
         continue
-
+    
+    # 打开图像
     img = Image.open(image_input_fullname)
     plt.figure("image_input_fullname")
     plt.subplot(1, 2, 1)
@@ -30,7 +35,7 @@ for each_image in os.listdir(IMAGE_INPUT_PATH):
     plt.axis('off')
 
     # 裁剪
-    roi_area = img.crop(BOX_LEFT, BOX_UP, BOX_RIGHT + BOX_LEFT, BOX_DOWN + BOX_UP)
+    roi_area = img.crop(BOX_LEFT, BOX_UP, BOX_RIGHT, BOX_DOWN)
     plt.subplot(1, 2, 2)
     plt.imshow(roi_area)
     plt.axis('off')
